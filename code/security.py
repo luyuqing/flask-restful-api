9f52af9c018d85355190b619c52bd1cdf3dfa385
Yuqing Lu <luyuqing0708@gmail.com>
2018-04-22 18:31:57 +0200

    Optimize code

--- code/security.py
+++ code/security.py
@@ -1,13 +1,6 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-    User(1, 'lala', '12345')
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
-
 def authenticate(username, password):
     user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
@@ -16,3 +9,9 @@ def authenticate(username, password):
 def identity(payload):
     user_id = payload['identity']
     return User.find_by_id(user_id)
+
+# users = [
+#     User(1, 'lala', '12345')
+# ]
+# username_mapping = {u.username: u for u in users}
+# userid_mapping = {u.id: u for u in users}
